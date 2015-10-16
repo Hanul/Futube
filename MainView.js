bf8b589ee22f9e3e73882183428cf6f393db7e5c
Hanul <hanul@hanul.me>
2015-10-16 12:55:37 +0900

    2차 업데이트

--- MainView.js
+++ MainView.js
@@ -1,8 +1,9 @@
 var Observable = require("FuseJS/Observable");
+var Secure = require("Secure");
 
 var pages = Observable();
 
-var mainPage = {title: "Futube", handle: "pagesList"};
+var mainPage = {title: "Futube", handle: "pagesList", isShowingSearchBox : false};
 
 var currentPage = Observable(mainPage);
 
@@ -22,28 +23,77 @@ function goBack(arg){
 	currentPage.value = mainPage;
 }
 
-fetch('https://www.googleapis.com/youtube/v3/videos?chart=mostPopular&key=AIzaSyDiS2hKjwlVrazb-8io3-z609XHF3Rwa5o&part=snippet,contentDetails,statistics,status', {
-    headers: { "Content-type": "application/json"}
-}).then(function(response) {
-    return response.json();    // This returns a promise
-}).then(function(responseObject) {
-    if (responseObject.error === undefined) {
-    	responseObject.items.forEach(function(itemInfo) {
-    		if (itemInfo.kind === 'youtube#video') {
-    			pages.add({title: itemInfo.snippet.title, handle: itemInfo.id, thumbnail: itemInfo.snippet.thumbnails.medium.url});
-    		}
-    	});
-    }
-}).catch(function(err) {
-    // An error occured parsing Json
-});
+var searchText = '';
 
-module.exports = {
-	pages: pages,
-	currentPage: currentPage,
-	currentPageHandle: currentPageHandle,
-	currentPageTitle: currentPageTitle,
-	pageButtonClicked: pageButtonClicked,
-	goBack: goBack
+function setSearchText(args) {
+    searchText = args.value;
 };
 
+function search() {
+
+    pages.clear();
+
+    if (searchText === '') {
+
+        fetch('https://www.googleapis.com/youtube/v3/videos?chart=mostPopular&key=' + Secure.youtubeKey + '&part=snippet&maxResults=10', {
+            headers: { "Content-type": "application/json"}
+        }).then(function(response) {
+            return response.json();    // This returns a promise
+        }).then(function(responseObject) {
+            if (responseObject.error === undefined) {
+                responseObject.items.forEach(function(itemInfo, i) {
+                    if (itemInfo.kind === 'youtube#video') {
+                        pages.add({
+                            duration : 0.5 + i * 0.1,
+                            title: itemInfo.snippet.title.length > 48 ? itemInfo.snippet.title.substring(0, 46) + '...' : itemInfo.snippet.title,
+                            description: itemInfo.snippet.description,
+                            channelTitle: itemInfo.snippet.channelTitle,
+                            handle: itemInfo.id,
+                            thumbnail: itemInfo.snippet.thumbnails.medium.url
+                        });
+                    }
+                });
+            }
+        }).catch(function(err) {
+            // An error occured parsing Json
+        });
+    }
+
+    else {
+        fetch('https://www.googleapis.com/youtube/v3/search?q=' + searchText + '&key=' + Secure.youtubeKey + '&part=snippet&maxResults=10', {
+            headers: { "Content-type": "application/json"}
+        }).then(function(response) {
+            return response.json();    // This returns a promise
+        }).then(function(responseObject) {
+            if (responseObject.error === undefined) {
+                responseObject.items.forEach(function(itemInfo, i) {
+                    if (itemInfo.id.kind === 'youtube#video') {
+                        pages.add({
+                            duration : 0.5 + i * 0.1,
+                            title: itemInfo.snippet.title.length > 48 ? itemInfo.snippet.title.substring(0, 46) + '...' : itemInfo.snippet.title,
+                            description: itemInfo.snippet.description,
+                            channelTitle: itemInfo.snippet.channelTitle,
+                            handle: itemInfo.id.videoId,
+                            thumbnail: itemInfo.snippet.thumbnails.medium.url
+                        });
+                    }
+                });
+            }
+        }).catch(function(err) {
+            // An error occured parsing Json
+        });
+    }
+};
+
+search();
+
+module.exports = {
+	pages : pages,
+	currentPage : currentPage,
+	currentPageHandle : currentPageHandle,
+	currentPageTitle : currentPageTitle,
+	pageButtonClicked : pageButtonClicked,
+	goBack : goBack,
+    setSearchText : setSearchText,
+    search : search
+};
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Futube
+
+Fuse를 이용한 유튜브 앱 만들기
+
+- http://series.hanul.so/series/view/561a1f5892d494c52eb9c266
+
+[퓨즈 스터디 페이스북 그룹](https://www.facebook.com/groups/fusestudy)
